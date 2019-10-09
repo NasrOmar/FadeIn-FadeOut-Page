b8d275126da406d18d27b78ec4eb02711db65f51
Omar Nasr <nasr.omar@icloud.com>
2019-10-09 03:25:31 +0200

    typing mistake removed

--- js/page-fader.js
+++ js/page-fader.js
@@ -1,6 +1,3 @@
-animation-duration: 300ms;
-    animation-timing-function: ease-in-out;// JavaScript Document
-
 function fadeInPage() {
 	var fader = document.getElementById("page-fader");
 	fader.classList.add("fade-in-page");
