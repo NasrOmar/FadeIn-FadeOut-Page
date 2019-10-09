827e9eae84345d206ba7c33b156a6b7d1ac9386f
Omar Nasr <nasr.omar@icloud.com>
2019-10-09 03:23:45 +0200

    Added content for page fade in

---
+++ js/page-fader.js
@@ -0,0 +1,7 @@
+animation-duration: 300ms;
+    animation-timing-function: ease-in-out;// JavaScript Document
+
+function fadeInPage() {
+	var fader = document.getElementById("page-fader");
+	fader.classList.add("fade-in-page");
+}
